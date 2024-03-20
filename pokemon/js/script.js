fe97a4b35def1c91aa159bc29bbb2f254e800953
Gabriel Cardoso <164118508+eucardoso0@users.noreply.github.com>
2024-03-20 00:03:13 -0300

    Add files via upload
    pokedex

---
+++ pokemon/js/script.js
@@ -0,0 +1,65 @@
+const pokemonName = document.querySelector('.pokemon_name');
+const pokemonNumber = document.querySelector('.pokemon_number');
+const pokemonImage = document.querySelector('.pokemon_image');
+
+const form = document.querySelector('.form');
+const input = document.querySelector('.input_search');
+const buttonVoltar = document.querySelector('.bnt-voltar');
+const buttonPróximo = document.querySelector('.bnt-proximo');
+
+let searchPokemon = 1;
+
+
+
+const fetchpokemon = async (pokemon) =>{
+
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+
+    if(APIResponse.status === 200){
+    const data = await APIResponse.json();
+    return data;
+    }
+
+} 
+
+const renderPokemon = async (pokemon) =>{
+
+    pokemonName.innerHTML = 'Carregando...';
+    pokemonNumber.innerHTML = '';
+
+    const data = await fetchpokemon(pokemon);
+
+    if(data){
+
+    pokemonImage.style.display = 'block';
+    pokemonName.innerHTML = data.name;
+    pokemonNumber.innerHTML = data.id;
+    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+
+    input.value = '';
+    } else{
+    pokemonImage.computedStyleMap.display = 'none';
+    pokemonName.innerHTML ='Não Encontrado :c';
+    pokemonNumber.innerHTML = '';
+    }
+}
+
+form.addEventListener('submit', (event) =>{
+    event.preventDefault()
+    renderPokemon(input.value.toLowerCase());
+   
+});
+
+buttonVoltar.addEventListener('click', () => {
+    if (searchPokemon > 1) {
+      searchPokemon -= 1;
+      renderPokemon(searchPokemon);
+    }
+  });
+
+  buttonProximo.addEventListener('click', () => {
+    searchPokemon += 1;
+    renderPokemon(searchPokemon);
+  });
+
+renderPokemon(searchPokemon);
